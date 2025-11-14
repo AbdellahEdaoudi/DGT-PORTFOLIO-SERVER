5b2cfa93ca921e7606164802c024d0faab284988
abdellah <abdellahedaoudi80@gmail.com>
2025-11-15 00:49:43 +0100

    fix: increase rate limit window for contact and links limiters

--- Limiting/contactLimiter.js
+++ Limiting/contactLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const ms = require('ms');
 
 exports.contactLimiter = rateLimit({
-    windowMs: ms('60m'),
+    windowMs: ms('1500m'),
     max: 5,
     message: 'You have exceeded the maximum number of messages allowed. Please try again later.',
     standardHeaders: true,
--- Limiting/linksLimiter.js
+++ Limiting/linksLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const ms = require('ms');
 
 exports.linksLimiter = rateLimit({
-    windowMs: ms('15m'),
+    windowMs: ms('1500m'),
     max: 10,
     message: 'You have exceeded the maximum number of messages allowed. Please try again later.',
     standardHeaders: true,
