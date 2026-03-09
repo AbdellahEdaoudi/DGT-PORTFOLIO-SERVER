e25a9c899643ea6e57c3e966263e8c09286c024c
abdellah <abdellahedaoudi80@gmail.com>
2026-03-09 00:50:37 +0000

    change database connection URL to MONGODB_ATLAS

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 exports.connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGODB_ATLAS);
         console.log(`Connected to MongoDB Atlas`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
